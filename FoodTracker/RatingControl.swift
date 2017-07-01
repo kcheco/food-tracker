4a844d601e0a8edaa5b5a62485bd597646186eaa
Kelvin Checo <checokelvin@gmail.com>
2017-07-01 01:35:38 -0400

    Added Interface Builder support for RatingControl

--- FoodTracker/RatingControl.swift
+++ FoodTracker/RatingControl.swift
@@ -8,6 +8,7 @@
 
 import UIKit
 
+@IBDesignable
 class RatingControl: UIStackView {
 
     /*
@@ -17,6 +18,18 @@ class RatingControl: UIStackView {
      */
     private var ratingButtons = [UIButton]()
     var rating = 0
+    
+    //property observers
+    @IBInspectable var starSize: CGSize = CGSize(width: 44.0, height: 44.0) {
+        didSet {
+            setupButtons()
+        }
+    }
+    @IBInspectable var starCount: Int = 5 {
+        didSet {
+            setupButtons()
+        }
+    }
     
     
     /*
@@ -41,8 +54,15 @@ class RatingControl: UIStackView {
      */
     private func setupButtons() {
         
+        // clear any exisiting buttons
+        for button in ratingButtons {
+            removeArrangedSubview(button)
+            button.removeFromSuperview()
+        }
+        ratingButtons.removeAll()
+        
         // Create 5 buttons (the _ is set since we don't need to know the current index iteration)
-        for _ in 0..<5 {
+        for _ in 0..<starCount {
             // creates a button
             let button = UIButton()
             
@@ -53,8 +73,8 @@ class RatingControl: UIStackView {
             button.translatesAutoresizingMaskIntoConstraints = false
             
             // activates the buttons height and width constraints
-            button.heightAnchor.constraint(equalToConstant: 44.0).isActive = true
-            button.widthAnchor.constraint(equalToConstant: 44.0).isActive = true
+            button.heightAnchor.constraint(equalToConstant: starSize.height).isActive = true
+            button.widthAnchor.constraint(equalToConstant: starSize.width).isActive = true
             
             // set up the button action
             button.addTarget(self, action: #selector(RatingControl.ratingButtonTapped(button:)), for: .touchUpInside)
