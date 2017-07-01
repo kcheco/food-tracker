b3579ee00862b5d8cfd90a4abaded7c7ab0052f6
Kelvin Checo <checokelvin@gmail.com>
2017-06-30 23:58:55 -0400

    Added a RatingConrtol class which inherits from UIStackView and set a view object to storyboard

---
+++ FoodTracker/RatingControl.swift
@@ -0,0 +1,25 @@
+//
+//  RatingControl.swift
+//  FoodTracker
+//
+//  Created by Kelvin Checo on 6/30/17.
+//  Copyright © 2017 Kelvin Checo. All rights reserved.
+//
+
+import UIKit
+
+class RatingControl: UIStackView {
+
+    /*
+    ------------------------------------
+     MARK: Initialization
+    ------------------------------------
+    */
+    override init(frame: CGRect) {
+        super.init(frame: frame)
+    }
+
+    required init(coder: NSCoder) {
+        super.init(coder: coder)
+    }
+}
