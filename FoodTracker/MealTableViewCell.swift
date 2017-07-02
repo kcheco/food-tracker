0eb457ab5f997114fa276b3088ac75e5f8d5b253
Kelvin Checo <checokelvin@gmail.com>
2017-07-02 13:05:30 -0400

    Added a table view controller and set the elements in the UI

---
+++ FoodTracker/MealTableViewCell.swift
@@ -0,0 +1,24 @@
+//
+//  MealTableViewCell.swift
+//  FoodTracker
+//
+//  Created by Kelvin Checo on 7/2/17.
+//  Copyright © 2017 Kelvin Checo. All rights reserved.
+//
+
+import UIKit
+
+class MealTableViewCell: UITableViewCell {
+
+    override func awakeFromNib() {
+        super.awakeFromNib()
+        // Initialization code
+    }
+
+    override func setSelected(_ selected: Bool, animated: Bool) {
+        super.setSelected(selected, animated: animated)
+
+        // Configure the view for the selected state
+    }
+
+}
