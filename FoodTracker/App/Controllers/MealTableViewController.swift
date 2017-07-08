a01ed74e28b69279203d0abfd0d2dfeef71d9345
Kelvin Checo <checokelvin@gmail.com>
2017-07-07 20:39:03 -0400

    updated app directory structure

---
+++ FoodTracker/App/Controllers/MealTableViewController.swift
@@ -0,0 +1,198 @@
+//
+//  MealTableViewController.swift
+//  FoodTracker
+//
+//  Created by Kelvin Checo on 7/2/17.
+//  Copyright © 2017 Kelvin Checo. All rights reserved.
+//
+
+import UIKit
+import os.log
+
+class MealTableViewController: UITableViewController {
+    
+    /*
+    ------------------------------------
+    MARK: Properties
+    ------------------------------------
+    */
+    // Sets a new Meal Object within the meal variable
+    var meals = [Meal]()
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Use the edit button item provided by the table view controller.
+        navigationItem.leftBarButtonItem = editButtonItem
+        
+        // Load the sample data.
+        loadSampleMeals()
+
+        // Uncomment the following line to preserve selection between presentations
+        // self.clearsSelectionOnViewWillAppear = false
+
+        // Uncomment the following line to display an Edit button in the navigation bar for this view controller.
+        // self.navigationItem.rightBarButtonItem = self.editButtonItem()
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+    /*
+    ------------------------------------
+    MARK: - Table view data source
+     
+    The following methods come with a TableViewController by default as par of its dataSource and delegate protocols
+    ------------------------------------
+    */
+    
+    // Setting this method to 1 allows for this tableView to appear in the view
+    override func numberOfSections(in tableView: UITableView) -> Int {
+        return 1
+    }
+
+    // This returns the total number of rows in dataSource
+    override func tableView(_ tableView: UITableView, numberOfRowsInSection section: Int) -> Int {
+        return meals.count
+    }
+
+    //
+    override func tableView(_ tableView: UITableView, cellForRowAt indexPath: IndexPath) -> UITableViewCell {
+        // Table view cells are reused and should be dequeued using a cell identifier.
+        let cellIdentifier = "MealTableViewCell"
+        
+        // Sets a constant that will downcast UITableViewCell to MealTableViewCell after being unwrapped. If there is an issue with mapping the MealTableViewCell then log error
+        guard let cell = tableView.dequeueReusableCell(withIdentifier: cellIdentifier, for: indexPath) as? MealTableViewCell else {
+            fatalError("The dequeued cell is not an instance of MealTableViewCell")
+        }
+        
+        // Fetches the appropriate meal for the data source layout.
+        let meal = meals[indexPath.row]
+
+        // Configure the cell by attaching the values within the corresponding item in the Meal Object to the views set in MealTableViewCell
+        cell.nameLabel.text = meal.name
+        cell.photoImageView.image = meal.photo
+        cell.ratingControl.rating = meal.rating
+
+        return cell
+    }
+
+    // Override to support conditional editing of the table view.
+    override func tableView(_ tableView: UITableView, canEditRowAt indexPath: IndexPath) -> Bool {
+        // Return false if you do not want the specified item to be editable.
+        return true
+    }
+
+    // Override to support editing the table view.
+    override func tableView(_ tableView: UITableView, commit editingStyle: UITableViewCellEditingStyle, forRowAt indexPath: IndexPath) {
+        if editingStyle == .delete {
+            // Delete the row from the data source
+            meals.remove(at: indexPath.row)
+            tableView.deleteRows(at: [indexPath], with: .fade)
+        } else if editingStyle == .insert {
+            // Create a new instance of the appropriate class, insert it into the array, and add a new row to the table view
+        }    
+    }
+
+    /*
+    // Override to support rearranging the table view.
+    override func tableView(_ tableView: UITableView, moveRowAt fromIndexPath: IndexPath, to: IndexPath) {
+
+    }
+    */
+
+    /*
+    // Override to support conditional rearranging of the table view.
+    override func tableView(_ tableView: UITableView, canMoveRowAt indexPath: IndexPath) -> Bool {
+        // Return false if you do not want the item to be re-orderable.
+        return true
+    }
+    */
+
+    /*
+    ------------------------------------
+    MARK: Navigation
+    ------------------------------------
+    */
+
+    // In a storyboard-based application, you will often want to do a little preparation before navigation
+    override func prepare(for segue: UIStoryboardSegue, sender: Any?) {
+        super.prepare(for: segue, sender: sender)
+        
+        switch(segue.identifier ?? "") {
+        
+            // if the user is adding an item, the view remains the same
+            case "AddItem":
+                os_log("Adding a new meal.", log: OSLog.default, type: .debug)
+            
+            // if the user selects a meal from the list, then look up specific Meal item and pass the object to mealView controller
+            case "ShowDetail":
+                guard let mealDetailViewController = segue.destination as? MealViewController else {
+                    fatalError("Unexpected destination: \(segue.destination)")
+                }
+            
+                guard let selectedMealCell = sender as? MealTableViewCell else {
+                    fatalError("Unexpected sender: \(String(describing: sender))")
+                }
+            
+                guard let indexPath = tableView.indexPath(for: selectedMealCell) else {
+                    fatalError("The selected cell is not being displayed by the table")
+                }
+            
+                let selectedMeal = meals[indexPath.row]
+                mealDetailViewController.meal = selectedMeal
+                os_log("Editing a meal", log: OSLog.default, type: .debug)
+            default:
+                fatalError("Unexpected Segue Identifier; \(String(describing: segue.identifier))")
+        }
+    }
+    
+    /*
+    ------------------------------------
+     MARK: Actions
+    ------------------------------------
+    */
+    @IBAction func unwindToMealList(sender: UIStoryboardSegue) {
+        // Detects if an instance of Meal is being passed through and assigns it to sourceViewController as if it was being referred to as MealViewController
+        if let sourceViewController = sender.source as? MealViewController, let meal = sourceViewController.meal {
+            
+            if let selectedIndexPath = tableView.indexPathForSelectedRow {
+                // Update an existing meal.
+                meals[selectedIndexPath.row] = meal
+                tableView.reloadRows(at: [selectedIndexPath], with: .none)
+            } else {
+                // if a new meal is set, then find the last index in the list of meals and add the new meal to the end of the table view
+                let newIndexPath = IndexPath(row: meals.count, section: 0)
+                
+                meals.append(meal)
+                tableView.insertRows(at: [newIndexPath], with: .automatic)
+            }
+        }
+    }
+    
+    /*
+    ------------------------------------
+    MARK: Private Methods
+    ------------------------------------
+    */
+    // This is considered a seed of a data model
+    private func loadSampleMeals() {
+        // load the three images that will be used for the sample data
+        let photo1 = UIImage(named: "meal1")
+        let photo2 = UIImage(named: "meal2")
+        let photo3 = UIImage(named: "meal3")
+        
+        guard let meal1 = Meal(name: "Caprese Salad", photo: photo1, rating: 4) else {
+            fatalError("Unable to initiate meal1")
+        }
+        guard let meal2 = Meal(name: "Chicken and Potatoes", photo: photo2, rating: 5) else {
+            fatalError("Unable to initiate meal2")
+        }
+        guard let meal3 = Meal(name: "Pasta with Meatballs", photo: photo3, rating: 3) else {
+            fatalError("Unable to initiate meal3")
+        }
+        
+        meals += [meal1, meal2, meal3]
+    }
+}
---
+++ FoodTracker/App/Controllers/MealViewController.swift
@@ -0,0 +1,169 @@
+//
+//  ViewController.swift
+//  FoodTracker
+//
+//  Created by Kelvin Checo on 6/29/17.
+//  Copyright © 2017 Kelvin Checo. All rights reserved.
+//
+
+import UIKit
+import os.log
+
+class MealViewController: UIViewController, UITextFieldDelegate, UIImagePickerControllerDelegate, UINavigationControllerDelegate {
+    
+    /*
+    ------------------------------------
+    MARK: Properties
+    ------------------------------------
+    */
+    @IBOutlet weak var nameTextField: UITextField!
+    @IBOutlet weak var photoImageView: UIImageView!
+    @IBOutlet weak var ratingControl: RatingControl!
+    @IBOutlet weak var saveButton: UIBarButtonItem!
+
+    var meal: Meal?
+    
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        
+        // Handle the text field’s user input through delegate callbacks.
+        nameTextField.delegate = self
+        
+        // Set up views if editing/viewing an existing Meal.
+        if let meal = meal {
+            navigationItem.title = meal.name
+            nameTextField.text   = meal.name
+            photoImageView.image = meal.photo
+            ratingControl.rating = meal.rating
+        }
+        
+        // Enable the Save button only if the text field has a valid Meal name.
+        updateSaveButtonState()
+    }
+
+    /* 
+    ------------------------------------
+     MARK: UITextFieldDelegate 
+    ------------------------------------
+     */
+    
+    // The text field should end its first-responder status given to it by the UIResponder once the user taps the Done button on the keyboard
+    func textFieldShouldReturn(_ textField: UITextField) -> Bool {
+        
+        // Hide the keyboard
+        textField.resignFirstResponder()
+        return true
+    }
+    
+    // Disable the Save button while editing
+    func textFieldDidBeginEditing(_ textField: UITextField) {
+        saveButton.isEnabled = false
+    }
+    
+    // Checks if text field is no longer empty and checks state of saveButton. If text field is no longer empty, then update the title of the view
+    func textFieldDidEndEditing(_ textField: UITextField) {
+        updateSaveButtonState()
+        navigationItem.title = textField.text
+    }
+
+    
+    /*
+    ------------------------------------
+    MARK: UIImagePickerControllerDelegate
+    ------------------------------------
+    */
+    // Informs controller when user taps the cancel button within image picker
+    func imagePickerControllerDidCancel(_ picker: UIImagePickerController) {
+        // Dismiss the picker if the user cancelled
+        dismiss(animated: true, completion: nil)
+    }
+    
+    // Informs controller when user selects an image from within the image picker
+    func imagePickerController(_ picker: UIImagePickerController, didFinishPickingMediaWithInfo info: [String : Any]) {
+        
+        // The info dictionary may contain multiple representations of the image. We will use an original image and set it as a UIImage object. If something goes wrong after unwrapped optional, then log error
+        guard let selectedImage = info[UIImagePickerControllerOriginalImage] as? UIImage else {
+            fatalError("Expected a dictionary containing an image, but was provided the following: /(info)")
+        }
+        
+        // Set photoImageView to display the selected image.
+        photoImageView.image = selectedImage
+        
+        dismiss(animated: true, completion: nil)
+    }
+    
+    /*
+     ------------------------------------
+     MARK: Navigation
+     ------------------------------------
+     */
+    // Returns user to MealTableView
+    @IBAction func cancelButton(_ sender: UIBarButtonItem) {
+        // Depending on style of presentation (modal or push presentation), this view controller needs to be dismissed in two different ways.
+        let isPresentingInAddMealMode = presentingViewController is UINavigationController
+        
+        // if user is in the add meal view and hits cancel, then return to list screen. Otherwise, if user has selected a meal from list and is in edit mode but hits the cancel button then send back to list view.
+        if isPresentingInAddMealMode {
+            dismiss(animated: true, completion: nil)
+        } else if let owningNavigationController = navigationController {
+            // checks the navigation stack and removes the view which then displays the previous screen (edit -> list)
+            owningNavigationController.popViewController(animated: true)
+        } else {
+            // this should never execute unless there is a bug
+            fatalError("The MealViewController is not inside a navigation controller.")
+        }
+    }
+    
+    // This method lets you configure a view controller before it's presented.
+    override func prepare(for segue: UIStoryboardSegue, sender: Any?) {
+        super.prepare(for: segue, sender: sender)
+        
+        // Configure the destination view controller only when the save button is pressed.
+        guard let button = sender as? UIBarButtonItem, button === saveButton else {
+            os_log("The save button was not pressed, cancelling", log: OSLog.default, type: .debug)
+            return
+        }
+        
+        let name = nameTextField.text ?? ""
+        let photo = photoImageView.image
+        let rating = ratingControl.rating
+        
+        // Set the meal to be passed to MealTableViewController after the unwind segue.
+        meal = Meal(name: name, photo: photo, rating: rating)
+    }
+    
+    /*
+    ------------------------------------
+    MARK: Actions
+    ------------------------------------
+    */
+    @IBAction func selectImageFromImageLibrary(_ sender: UITapGestureRecognizer) {
+        
+        // Ensures the keyboard is dismissed if user has previously interacted with the text field and has then tapped the ImageView
+        nameTextField.resignFirstResponder()
+        
+        // UIImagePickerController is a view controller that lets a user pick media from their photo library
+        let imagePickerController = UIImagePickerController()
+        
+        // Only allow photos to be picked, not taken
+        imagePickerController.sourceType = .photoLibrary
+        
+        
+        // Make sure ViewController is notified when the user picks an image.
+        imagePickerController.delegate = self
+        present(imagePickerController, animated: true, completion: nil)
+    }
+    
+    /*
+     ------------------------------------
+     MARK: Private Methods
+     ------------------------------------
+     */
+    // Disable the Save button if the text field is empty
+    private func updateSaveButtonState() {
+        let text = nameTextField.text ?? ""
+        saveButton.isEnabled = !text.isEmpty
+    }
+    
+}
+
